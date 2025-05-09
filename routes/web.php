fe9fd773452957ff75883d96e5f4b6e5d498adf9
pujaa15 <pujawati150904@gmail.com>
2025-05-09 11:48:59 +0700

    update semua perubahan terbaru

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[AuthController::class, 'index']);
+Route::get('/register',[AuthController::class,'registerpage']);
+
+Route::group(['prefix' => 'panel-control'], function(){
+    Route::get('/dashboard', [DashboardController::class, 'index']);
 });
+
+
